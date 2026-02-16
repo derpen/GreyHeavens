738de9a2141bad79c58de1e5cca9e09a1a9e80aa
derpen <rizqyassabil@hotmail.com>
2026-02-17 03:07:06 +0700

    CMake shenanigans, and a blue colored screen.

--- core/main.cpp
+++ core/main.cpp
@@ -13,17 +13,50 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+#include <glad/glad.h>
+#include <iostream>
+
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+static SDL_GLContext maincontext;
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
+    SDL_GL_LoadLibrary(NULL);
+	SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+	// Request a depth buffer
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
     /* Create the window */
-    if (!SDL_CreateWindowAndRenderer("Hello World", 800, 600, SDL_WINDOW_FULLSCREEN, &window, &renderer)) {
+    window = SDL_CreateWindow("Grey Heavens", 800, 600, SDL_WINDOW_FULLSCREEN | SDL_WINDOW_OPENGL);
+    if (!window) {
         SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
+
+    maincontext = SDL_GL_CreateContext(window);
+    if (!maincontext) {
+        SDL_Log("Couldn't create context: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+    }
+
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
+	{
+        SDL_Log("Couldn't get ProcAddres: %s", SDL_GetError());
+		return SDL_APP_FAILURE;
+	}
+
+    // Sync to monitors refresh rate, idk
+    SDL_GL_SetSwapInterval(1);
+
+    int w,h;
+    SDL_GetWindowSize(window, &w, &h);
+    glViewport(0, 0, w, h);
+
     return SDL_APP_CONTINUE;
 }
 
@@ -40,23 +73,10 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    const char *message = "Hello World!";
-    int w = 0, h = 0;
-    float x, y;
-    const float scale = 4.0f;
+    SDL_GL_SwapWindow(window);
 
-    /* Center the message and scale it up */
-    SDL_GetRenderOutputSize(renderer, &w, &h);
-    SDL_SetRenderScale(renderer, scale, scale);
-    x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(message)) / 2;
-    y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
-
-    /* Draw the message */
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderDebugText(renderer, x, y, message);
-    SDL_RenderPresent(renderer);
+    glClearColor(0.0f, 0.5f, 1.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
     return SDL_APP_CONTINUE;
 }
